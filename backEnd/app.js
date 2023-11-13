8267db508d2a59831752a33c33e20e01be5c4627
Amruth <amruhtkrishnaa@gmail.com>
2023-11-13 19:31:09 +0530

    authentication token headers added

--- backEnd/app.js
+++ backEnd/app.js
@@ -8,8 +8,10 @@ const port = process.env.PORT
 
 const app = express();
 app.use(body_parser.json())
-app.use((req, res, next) => {
+e((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'https://talk-scape.onrender.com');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, authentication'); // Add 'authentication' to the list of allowed headers
     next();
   });
   
