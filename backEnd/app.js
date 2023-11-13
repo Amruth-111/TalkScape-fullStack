b097c39f8469de09968410dc9697218afe174a69
Amruth <amruhtkrishnaa@gmail.com>
2023-11-13 18:54:30 +0530

    backend socket connections done

--- backEnd/app.js
+++ backEnd/app.js
@@ -56,7 +56,7 @@ const server = app.listen(port)
 const io = require('socket.io')(server, {
     pingTimeout: 80000,
     cors: {
-        origin: 'http://localhost:3000'
+        origin: 'https://talk-scape.onrender.com/'
     },
 
 })
